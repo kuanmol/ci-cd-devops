abc4768763e3c67bb905489d69186dcba3d01553
kuanmol <ramuklomna100@gmail.com>
2026-02-27 19:36:43 +0530

    feat: add microservice

---
+++ user-service/app/main.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+
+app = FastAPI()
+
+@app.get("/")
+def root():
+    return {"message": "User Service Running"}
+
+@app.get("/users/{user_id}")
+def get_user(user_id: int):
+    return {
+        "user_id": user_id,
+        "name": "Anmol",
+        "email": "anmol@example.com"
+    }
+
+@app.get("/health")
+def health():
+    return {"status": "ok"}
