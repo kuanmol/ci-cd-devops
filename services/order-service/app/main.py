5bd9b2c2e049b8c3f886fc0470a0dc81f1b72aa7
kuanmol <ramuklomna100@gmail.com>
2026-02-28 21:28:04 +0530

    fix: changing port

--- services/order-service/app/main.py
+++ services/order-service/app/main.py
@@ -3,10 +3,10 @@ import requests
 
 app = FastAPI()
 
-USER_SERVICE = "http://user-service"
-PRODUCT_SERVICE = "http://product-service"
-PAYMENT_SERVICE = "http://payment-service"
-NOTIFICATION_SERVICE = "http://notification-service"
+USER_SERVICE = "http://user-service:8000"
+PRODUCT_SERVICE = "http://product-service:8000"
+PAYMENT_SERVICE = "http://payment-service:8000"
+NOTIFICATION_SERVICE = "http://notification-service:8000"
 
 @app.get("/")
 def root():
